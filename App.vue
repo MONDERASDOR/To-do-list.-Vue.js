6c59c7053e2f6c4d60c5725f7839350d9032b343
MONDERASDOR <140967226+MONDERASDOR@users.noreply.github.com>
2024-09-24 12:23:49 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <input v-model="newTodo" placeholder="Add a new todo" />
+    <button @click="addTodo">Add</button>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo.text }} <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim()) {
+        this.todos.push({ text: this.newTodo });
+        this.newTodo = '';
+      }
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+  },
+};
+</script>
